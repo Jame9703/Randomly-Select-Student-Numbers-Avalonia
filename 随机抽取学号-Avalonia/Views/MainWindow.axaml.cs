87f5a2d0c69905db01057d66cf732046d960e6b9
Jame9703 <ncsb1213444@outlook.com>
2025-04-04 22:16:23 +0800

    #

--- 随机抽取学号-Avalonia/Views/MainWindow.axaml.cs
+++ 随机抽取学号-Avalonia/Views/MainWindow.axaml.cs
@@ -1,8 +1,9 @@
 ﻿using Avalonia.Controls;
+using SukiUI.Controls;
 
 namespace 随机抽取学号_Avalonia.Views;
 
-public partial class MainWindow : Window
+public partial class MainWindow : SukiWindow
 {
     public MainWindow()
     {
