1616d6f68609bea9b8cd9848dc3ba7f25601e228
Jame9703 <ncsb1213444@outlook.com>
2025-04-19 10:21:49 +0800

    #

---
+++ 随机抽取学号-Avalonia/Views/HomeView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace 随机抽取学号_Avalonia;
+
+public partial class HomeView : UserControl
+{
+    public HomeView()
+    {
+        InitializeComponent();
+    }
+}
